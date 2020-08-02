65bd5c5fa1d6c0de998fd4a58a497ca0f924023b
Seunggyun Han <hsg8234@naver.com>
2020-08-02 03:16:06 -0400

    update BooksAPI.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    console.log(BooksAPI.getAll);
+    BooksAPI.getAll().then((books) => {
+      console.log(books);
+      this.setState(() => ({
+        books: books,
+      }));
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-header__title">My Reads: A Book Tracking App</h1>
         </header>
+        <main></main>
       </div>
     );
   }
